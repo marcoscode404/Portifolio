f10b5daf1d92d4e58c4d7a6878a211251acb5381
marcos gomes <marcos.gomes@modenaesilva.com.br>
2023-07-29 13:01:31 -0400

    feat: criando projeto blog

---
+++ blog/app.vue
@@ -0,0 +1,12 @@
+<script setup>
+let open = ref(false)
+</script>
+<template>
+  <header v-if="open">
+    Meu Blog
+  </header>
+  <button @click="open = true">
+    abrir
+  </button>
+  <!-- <NuxtPage /> -->
+</template>
