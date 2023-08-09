f5977a07fe6d64764657b0ff8995e120dc06028a
marcos gomes <marcos.gomes@modenaesilva.com.br>
2023-08-09 13:24:16 -0400

    save

--- blog/src/components/Global/Modal.vue
+++ blog/src/components/Global/Modal.vue
@@ -12,7 +12,10 @@ const emit = defineEmits(['close'])
     <Dialog
         class="m-auto p-3 px-5 w-full absolute flex justify-center items-center z-auto bg-[#1212122e] backdrop-blur-sm h-screen top-0"
         :open="$props.open" @close="emit('close')">
-        <DialogPanel class="flex w-fit flex-col rounded-lg justify-between items-center bg-white container">
+        <DialogPanel class="flex flex-col rounded-lg justify-between items-center container w-96 h-52
+            bg-gradient-to-r from-[#be78d333] from-10% via-yellow-50 via-50% to-[#fcfafa2d] to-90% 
+            shadow-2xl border
+        ">
 
             <div class="inline-flex justify-between w-full">
                 <div class="w-full text-center font-medium">
@@ -20,10 +23,8 @@ const emit = defineEmits(['close'])
                     </slot>
                 </div>
                 <div>
-                    <Icon @click="emit('close')"
-                        name="mdi:close"
-                        class="text-red-500 m-1 border border-red-600 rounded-full text-xl cursor-pointer hover:bg-red-600 hover:text-white"
-                    >
+                    <Icon @click="emit('close')" name="mdi:close"
+                        class="text-red-500 m-1 border border-red-600 rounded-full text-xl cursor-pointer hover:bg-red-600 hover:text-white">
                     </Icon>
                 </div>
             </div>
@@ -31,7 +32,7 @@ const emit = defineEmits(['close'])
             <DialogDescription class="flex justify-center items-center font-medium w-full h-full text-center">
                 <slot name="description"></slot>
             </DialogDescription>
-            
+
             <div class="m-2">
                 <slot></slot>
             </div>
--- blog/src/pages/index.vue
+++ blog/src/pages/index.vue
@@ -5,22 +5,26 @@ const isOpen = ref(false)
 <template>
   <main class="overflow-x-hidden">
     <div class="img">
-      <UModal v-model="isOpen" :overlay="false">
-        <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
-          <UButton :ui="{ rounded: 'rounded-full' }" @click="isOpen = false" color="red" class="text-white" label="X" />
-          Portifolio Em Construção!
-        </UCard>
-      </UModal>
+
+      <GlobalModal :open="isOpen" @close="isOpen = false">
+        <template #description>
+            <p class=" font-bold">Portifolio em Construção!</p>
+        </template>
+      </GlobalModal>
+
+
+
+
 
       <!-- banner -->
       <div class="h-full flex flex-col justify-center  max-w-7xl m-auto ">
 
-        <div class="rounded-full border-2 overflow-hidden border-white h-44 w-44 bg-white">
+        <div class="rounded-full border-2 overflow-hidden border-white flex justify-center mb-2 mx-auto h-44 w-44 bg-white">
           <img src="../assets/perfil.jpeg" alt="">
         </div>
 
         <div class="box-texto mt-2">
-          <span class="font-bold">Olá, Eu Sou</span>
+          <span class="font-bold text-white">Olá, Eu Sou</span>
           <h1 class="title font-bold h-16 flex items-center justify-center text-lg">Marcos Vini</h1>
           <span class="descricao">Desenvolvedor Front-end & UI Designer.</span>
 
@@ -88,11 +92,11 @@ background: red;
 
 font-size: 70px;
 font-weight: 700;
-color: #22cad0;
+color: white;
 font-family: 'open_sansregular';
 letter-spacing: 5px;
 margin-bottom: 20px;
-background:#ffffff57;
+/* background:#ffffff57; */
 position: relative;
 animation: motion 3s 1;
 }
